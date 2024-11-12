644ad837be99ae8c769d90e7e4adc54297451190
TheManBehind-Code <themanbehindcode@gmail.com>
2024-11-12 22:06:55 +0100

    Add Prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,17 @@
-import js from "@eslint/js";
+import js from '@eslint/js'
+import prettierRecommended from 'eslint-plugin-prettier/recommended'
 
 export default [
   js.configs.recommended,
+  prettierRecommended,
   {
-    files: ["src/**/*.js"],
-    ignores: ["**/*.test.js", ".config/*"],
+    files: ['src/**/*.js'],
+    ignores: ['**/*.test.js', '.config/*'],
     rules: {
-        semi: "warn",
-        "prefer-const": "warn",
-        "no-unused-vars": "warn",
-        "no-undef": "warn"
-    }
-  }
-];
+      semi: 'warn',
+      'prefer-const': 'warn',
+      'no-unused-vars': 'warn',
+      'no-undef': 'warn',
+    },
+  },
+]
