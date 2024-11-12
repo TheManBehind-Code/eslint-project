3bff68552a8b2c320bde0c6839b083febd198e67
TheManBehind-Code <themanbehindcode@gmail.com>
2024-11-12 22:11:48 +0100

    Add other rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,29 @@
 import js from "@eslint/js";
+import globals from 'globals';
 
 export default [
   js.configs.recommended,
+
   {
     files: ["src/**/*.js"],
     ignores: ["**/*.test.js", ".config/*"],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      }
+    },
     rules: {
-        semi: "warn",
-        "prefer-const": "warn",
-        "no-unused-vars": "warn",
-        "no-undef": "warn"
+      semi: "warn",
+      "prefer-const": "warn",
+      "no-unused-vars": "warn",
+        "no-undef": "error",
+      "eqeqeq": "error",
+      "no-var": "error",
+      "no-console": "warn",
+      "no-duplicate-imports": "error",
+      "no-template-curly-in-string": "error",
+      "camelcase": "warn",
+      "max-len": ["warn", { "code": 100 }]
     }
   }
 ];
